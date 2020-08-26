561f5a0f5c723616fb36c16050932d34f6f8b5a7
feel <feel@pop-os.localdomain>
2020-08-26 05:24:57 -0500

    removed layout script

--- layout.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-for i in /home/feel/Documents/Programming/cheatsheets/*.md; do
-    if $(grep -q 'layout: 2017/sheet' $i); then
-        echo "Found layout "; else
-        echo "No layout found ";
-        sed -i '3ilayout: 2017/sheet' $i;
-    fi
-done
